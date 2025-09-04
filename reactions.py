e515e514197090a6ffd4fe59c666ceed102036ea
CliNetCat <91659371+ClientBeta@users.noreply.github.com>
2025-09-04 15:06:39 +0800

    reactions.py

--- reactions.py
+++ reactions.py
@@ -1,21 +1,5 @@
 # reactions.py
-# "hello" : "https://static.wikia.nocookie.net/projectsekai/images/a/ac/Stamp0426.png/revision/latest?cb=20220117210321",
 reaction_data = {
-    "wow": "https://static.wikia.nocookie.net/projectsekai/images/9/9c/Stamp0007.png/revision/latest?cb=20220208072153",
-    "err": "https://static.wikia.nocookie.net/projectsekai/images/8/83/Stamp0005.png/revision/latest?cb=20211217165547",
-    "wonderhoy": "https://static.wikia.nocookie.net/projectsekai/images/2/2f/Stamp0168.png/revision/latest?cb=20211221151857",
-    "calm_down": "static.wikia.nocookie.net/projectsekai/images/c/c8/Stamp0569.png/revision/latest?cb=20231001195311",
-    "mission_accomplished": "https://static.wikia.nocookie.net/projectsekai/images/6/6b/Stamp0630.png/revision/latest?cb=20240519060019",
-    "i_belive_in_yall" : "https://static.wikia.nocookie.net/projectsekai/images/2/24/Stamp0425.png/revision/latest?cb=20220224225804",
-    "boohoo" : "https://static.wikia.nocookie.net/projectsekai/images/8/8d/Stamp0432.png/revision/latest?cb=20220407234335",
-    "you_promised" : "https://static.wikia.nocookie.net/projectsekai/images/9/94/Stamp0025.png/revision/latest?cb=20211221151943",
-    "cant belive this is happening" : "https://static.wikia.nocookie.net/projectsekai/images/2/20/Stamp0505.png/revision/latest?cb=20221208015631",
-    "lets have fun":"https://static.wikia.nocookie.net/projectsekai/images/c/c3/Stamp0520.png/revision/latest?cb=20221208020101",
-    "gasp":"https://static.wikia.nocookie.net/projectsekai/images/5/5b/Stamp0109.png/revision/latest?cb=20220224083203",
-    "wait for me":"https://static.wikia.nocookie.net/projectsekai/images/b/b6/Stamp0610.png/revision/latest?cb=20231109070036",
-    "Robo-Nene-Stare":"https://static.wikia.nocookie.net/projectsekai/images/6/63/Stamp0179.png/revision/latest?cb=20201030033436",
-    "kanade noodles":"https://static.wikia.nocookie.net/projectsekai/images/2/2b/Stamp0199.png/revision/latest?cb=20220225010710",
-    "Unknown-CN emote":"https://static.wikia.nocookie.net/projectsekai/images/8/84/Stamp30002.png/revision/latest?cb=20250711183149",
-    "Decapricated":"This emote doesn't exist, you are just curious what will be the outcome lol",
-    "gimme_bluray_please" : "https://i.ibb.co/3mSW90dZ/Haruka-11-prsk-erica-moe.png"
+    "test": "https://web.website.com/image.png",
+    "test2": "https://web.website.com/image.jpg",
 }
